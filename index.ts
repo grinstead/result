6245fd4f6f15fd798a609e65d9fbb150970ee73c
Joe Grinstead <npm@mailforce.net>
2024-07-06 22:33:44 -0400

    Custom make this npm interaction stuff.
    I don't like it, jeez

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+export type Success<T> = {
+  done: true;
+  value: T;
+  failure: undefined;
+};
+
+export type Failure<E extends {}> = {
+  done: true;
+  value: never;
+  failure: E;
+};
+
+export type Result<T, E extends {}> = Success<T> | Failure<E>;
+
+export function success<T>(value: T): Success<T> {
+  return { done: true, value, failure: undefined };
+}
+
+export function failure<E extends {}>(error: E): Failure<E> {
+  return {
+    done: true,
+    // @ts-expect-error
+    get value() {
+      throw error;
+    },
+    failure: error,
+  };
+}
